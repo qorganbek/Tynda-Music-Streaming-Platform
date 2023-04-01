65bbd7b4afc6053bebbb48bc931ad1bfe3ee0501
qorganbek <dimashqorganbek@gmail.com>
2023-04-01 22:12:47 +0600

    123

--- main/serializer.py
+++ main/serializer.py
@@ -2,14 +2,35 @@ from rest_framework import serializers
 from . import models
 
 
+class _CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = ['name']
+
+
+class _ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Artist
+        fields = ['username']
+
+
 class SongSerializer(serializers.ModelSerializer):
+    artist = _ArtistSerializer(read_only=True, many=True)
+    category = _CategorySerializer(read_only=True)
+
     class Meta:
         model = models.Song
         fields = '__all__'
 
 
+class _SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Song
+        fields = ['title']
+
+
 class ArtistSerializer(serializers.ModelSerializer):
-    songs = SongSerializer(read_only=True, many=True)
+    songs = _SongSerializer(read_only=True, many=True)
 
     class Meta:
         model = models.Artist
@@ -17,7 +38,7 @@ class ArtistSerializer(serializers.ModelSerializer):
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    songs = SongSerializer(read_only=True, many=True)
+    songs = _SongSerializer(read_only=True, many=True)
 
     class Meta:
         model = models.Category
