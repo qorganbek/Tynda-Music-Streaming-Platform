45d420fbe969f23de6658aa73cbcab874cb0edde
qorganbek <dimashqorganbek@gmail.com>
2023-04-01 20:40:14 +0600

    new

--- README.md
+++
@@ -1,1 +0,0 @@
-# Tynda-Music-Streaming-Platform
--- audio/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from . import models
-from . import serializer
-
-
-class AudioViewSet(ModelViewSet):
-    queryset = models.Audio.objects.all()
-    serializer_class = serializer.AudioSerializer
