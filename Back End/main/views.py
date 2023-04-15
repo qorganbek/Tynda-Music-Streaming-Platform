6c259d4ee109f0dd6463b999a5f1abac0f19e013
qorganbek <dimashqorganbek@gmail.com>
2023-04-15 15:31:08 +0600

    upd

--- Back End/main/views.py
+++ Back End/main/views.py
@@ -10,28 +10,28 @@ class SongModelViewSet(ModelViewSet):
     queryset = models.Song.objects.select_related('category')
     serializer_class = serializer.SongSerializer
     filterset_class = filters.SongFilter
-    permission_classes = (permissions.IsAdminOrReadOnly,)
+    # permission_classes = (permissions.IsAdminOrReadOnly,)
 
 
 class ArtistModelViewSet(ModelViewSet):
     queryset = models.Artist.objects.prefetch_related('song')
     serializer_class = serializer.ArtistSerializer
     filterset_class = filters.ArtistFilter
-    permission_classes = (permissions.IsAdminOrReadOnly,)
+    # permission_classes = (permissions.IsAdminOrReadOnly,)
 
 
 class CategoryModelViewSet(ModelViewSet):
     queryset = models.Category.objects.prefetch_related('song')
     serializer_class = serializer.CategorySerializer
     filterset_class = filters.CategoryFilter
-    permission_classes = (permissions.IsAdminOrReadOnly,)
+    # permission_classes = (permissions.IsAdminOrReadOnly,)
 
 
 class FavoriteModelViewSet(ModelViewSet):
     queryset = models.Favorite.objects.all()
     serializer_class = serializer.FavoriteSerializer
     filterset_class = filters.FavoriteFilter
-    permission_classes = (rest_permissions.DjangoObjectPermissions,)
+    # permission_classes = (rest_permissions.DjangoObjectPermissions,)
 
 
 class PlaylistModelViewSet(ModelViewSet):
